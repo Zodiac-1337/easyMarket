5e33afc41fe5bf4818fa091756d7192382684e41
Lyudov Maxim <maksluydov56@gmail.com>
2026-02-15 18:30:45 +1100

    fix ESLint

--- src/shared/i18n/core/getByPath.ts
+++ src/shared/i18n/core/getByPath.ts
@@ -1,7 +1,9 @@
 export const getByPath = (obj: unknown, path: string): unknown => {
   return path.split(".").reduce<unknown>((acc, key) => {
-    if (acc && typeof acc === "object" && key in (acc as any))
-      return (acc as any)[key];
+    if (acc && typeof acc === "object" && acc !== null) {
+      const record = acc as Record<string, unknown>;
+      return record[key];
+    }
     return undefined;
   }, obj);
 };
